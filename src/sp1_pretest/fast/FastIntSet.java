953eeb630e13d1b521733b22ccaeed32af52a843
Yuxuan Chen <chenyx0512@gmail.com>
2024-01-15 14:53:48 -0500

    Merged pathing WIP

---
+++ src/sp1_pretest/fast/FastIntSet.java
@@ -0,0 +1,41 @@
+package sp1_pretest.fast;
+
+public class FastIntSet {
+    public StringBuilder keys;
+    public int size;
+
+    public FastIntSet() {
+        keys = new StringBuilder();
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add(int i) {
+        String key = String.valueOf((char) i);
+        if (keys.indexOf(key) < 0) {
+            keys.append(key);
+            size++;
+        }
+    }
+
+    public void remove(int i) {
+        String key = String.valueOf((char) i);
+        int index;
+        if ((index = keys.indexOf(key)) >= 0) {
+            keys.deleteCharAt(index);
+            size--;
+        }
+    }
+
+    public boolean contains(int i) {
+        return keys.indexOf(String.valueOf((char) i)) >= 0;
+    }
+
+    public void clear() {
+        size = 0;
+        keys = new StringBuilder();
+    }
+}
