ec4f16021f275517b4e812d7dfa2db54134036d9
dsjong <dsjong2002@yahoo.com>
2024-01-15 09:45:55 -0500

    add turndir wip

--- src/pathing_test/PathFinder.java
+++ src/pathing_test/PathFinder.java
@@ -2,6 +2,7 @@ package pathing_test;
 
 import battlecode.common.*;
 import pathing_test.fast.FastIntSet;
+import pathing_test.fast.FastMath;
 
 class DirectionStack {
     static int STACK_SIZE = 60;
@@ -57,9 +58,14 @@ public class PathFinder {
         static DirectionStack dirStack = new DirectionStack();
         static MapLocation prevTarget = null; // previous target
         static int currentTurnDir = 0;
+        static int MAX_DEPTH = 15;
 
         static Direction turn(Direction dir) {
             return currentTurnDir == 0 ? dir.rotateLeft() : dir.rotateRight();
+        }
+
+        static Direction turn(Direction dir, int turnDir) {
+            return turnDir == 0 ? dir.rotateLeft() : dir.rotateRight();
         }
 
         static void move() {
@@ -88,6 +94,7 @@ public class PathFinder {
                         rc.move(dirR);
                         return;
                     }
+                    currentTurnDir = getTurnDir(dir);
                     // obstacle encountered, rotate and add new dirs to stack
                     while (!rc.canMove(dir) && dirStack.size < 8) {
                         if (!rc.onTheMap(rc.getLocation().add(dir))) {
@@ -106,7 +113,6 @@ public class PathFinder {
                     }
                 }
                 else {
-                    // TODO: add turn dir function call here
                     // TODO: don't understand this (why pop 2?)
                     if (dirStack.size > 1 && rc.canMove(dirStack.top(2))) {
                         dirStack.pop(2);
@@ -149,9 +155,69 @@ public class PathFinder {
             }
         }
 
+        static int simulate(int turnDir, Direction dir) throws GameActionException {
+            MapLocation now = rc.getLocation();
+            DirectionStack dirStack = new DirectionStack();
+            while (!canPass(now.add(dir), dir) && dirStack.size < 8) {
+                dirStack.push(dir);
+                dir = turn(dir, turnDir);
+            }
+            now = now.add(dir);
+
+            int ans = 0;
+            while (dirStack.size > 0) {
+                ans++;
+                if (ans > MAX_DEPTH) {
+                    break;
+                }
+                while (dirStack.size > 0 && canPass(now.add(dirStack.top()), dirStack.top())) {
+                    dirStack.pop();
+                }
+                // is reference code correct?
+                if (dirStack.size > 1 && canPass(now.add(dirStack.top()), dirStack.top(2))) {
+                    dirStack.pop(2);
+                }
+
+                Direction curDir;
+                while (dirStack.size > 0 && !canPass(now.add(curDir = turn(dirStack.top(), turnDir)), curDir)) {
+                    dirStack.push(curDir);
+                    if (dirStack.size > 8) {
+                        return -1;
+                    }
+                }
+                if (dirStack.size > 8 || dirStack.size == 0) {
+                    break;
+                }
+                Direction moveDir = dirStack.size == 0 ? dirStack.dirs[0] : turn(dirStack.top());
+                now = now.add(moveDir);
+            }
+            return ans + Util.distance(now, target);
+        }
+
+        static int getTurnDir(Direction dir) throws GameActionException {
+            int ansL = simulate(0, dir);
+            int ansR = simulate(1, dir);
+            if (ansL == -1 || ansR == -1) return FastMath.rand256() % 2;
+            if (ansL <= ansR) {
+                return 0;
+            }
+            else {
+                return 1;
+            }
+        }
+
         // clear some of the previous data
         static void resetPathfinding() {
             dirStack.clear();
+        }
+
+        // TODO: YUXUAN
+        static boolean canPass(MapLocation loc, Direction targetDir) throws GameActionException {
+            if (loc.equals(rc.getLocation())) return true;
+            // TODO: YUXUAN IMPL MAPRECORDR CHECK
+            if (!MapRecorder.check(loc.add(targetDir))) return false;
+            // TODO: YUXUAN CHECK IF SQRT 20
+            return rc.sensePassability(loc.add(targetDir));
         }
     }
 }
